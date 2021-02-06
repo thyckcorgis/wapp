9c52b05f824baa60409d925685d2c6b42c069281
wchorkaw <wchorkaw@ualberta.ca>
2021-02-06 11:35:11 -0700

    added react navigation

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 export default function App() {
   return (
     <View style={styles.container}>

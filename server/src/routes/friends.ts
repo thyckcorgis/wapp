c0b3c9221064797141c3345639894c4c47e979d2
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 15:13:42 -0700

    create friends route

--- server/src/routes/friends.ts
+++ server/src/routes/friends.ts
@@ -3,6 +3,8 @@ import users from "../userdb";
 
 const router = Router();
 
+router.post("/request", (req, res) => {});
+
 router.get("/", (_, res) => {
   res.json({ users: users.getAllUsers() });
 });

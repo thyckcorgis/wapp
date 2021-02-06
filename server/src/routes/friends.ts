8cd839ebd95914c1acc1dd81bbdf46bbb458df20
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 15:32:58 -0700

    add friends

--- server/src/routes/friends.ts
+++ server/src/routes/friends.ts
@@ -3,7 +3,35 @@ import users from "../userdb";
 
 const router = Router();
 
-router.post("/request", (req, res) => {});
+interface FriendReq {
+  username: string;
+  friend: string;
+}
+
+function sendFriendRequest(username: string, friend: string) {
+  console.log("Send friend request here");
+  console.log({ username, friend });
+}
+/*
+body:
+{
+  username: string,
+  friend: string
+}
+*/
+router.post("/request", (req, res) => {
+  const { username, friend } = req.body as FriendReq;
+  // send notification to friend
+  sendFriendRequest(username, friend);
+  res.json({ ok: true, message: "Request sent" });
+});
+
+router.post("/accept", (req, res) => {
+  const { username, friend } = req.body as FriendReq;
+  // send notification to
+  users.connectFriends(username, friend);
+  res.json({ ok: true, message: "Added friend" });
+});
 
 router.get("/", (_, res) => {
   res.json({ users: users.getAllUsers() });

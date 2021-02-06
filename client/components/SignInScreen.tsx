14702fe72baa61e8381457e2b3d923d20957f0d6
wchorkaw <wchorkaw@ualberta.ca>
2021-02-06 12:56:51 -0700

    sign in

--- client/components/SignInScreen.tsx
+++ client/components/SignInScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, TextInput } from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
+import { useState } from "react";
 
 
 interface SignInScreen {
@@ -9,13 +10,29 @@ interface SignInScreen {
   
 
 export default function SignInScreen({ navigation }: SignInScreen) {
+    const [ username, setUsername] = useState('');
+    const [ password, setPassword] = useState('');
+
     return (
         <View>
+            <Text style={{padding:50}}>This is the sign in screen</Text>
+            <TextInput
+                placeholder="Username"
+                onChangeText={(text) => setUsername(text)}
+                value={username}
+            />
+            <TextInput
+                placeholder="Password"
+                onChangeText={(text) => setPassword(text)}
+                value={password}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate("Welcome")}>
+                <Text style={{padding:50}}>Log In</Text>
+            </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-                <Text style={{padding:50}}>This is the Sign In screen</Text>
-            </TouchableOpacity>        
+                <Text style={{padding:50}}>Don't have an account? Sign UP!</Text>
+            </TouchableOpacity>
         </View>
-
     )
 }
 

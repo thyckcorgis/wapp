0ed83e3f37c49920545f3310decd079f678511ce
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 17:12:39 -0700

    wot

--- wapp/assets/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as StartIcon } from "./start_icon.svg";
-export { default as WelcomeIcon } from "./welcome_icon.svg";
--- wapp/styles/colours.tsx
+++
@@ -1,7 +0,0 @@
-export default {
-  yellow: "#fafdcb",
-  lightBlue: "#aee7e8",
-  medBlue: "#28c3d4",
-  darkBlue: "#248ea9",
-  errorRed: "#ef5a5a",
-};

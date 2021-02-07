344efd2db35c00ff30d9df56e7a77fe53bf1164f
wchorkaw <wchorkaw@ualberta.ca>
2021-02-06 20:42:18 -0700

    ?????

--- client/components/UserScreen.tsx
+++ client/components/UserScreen.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, TextInput } from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import { storeData } from "../storage";
+import { useState } from 'react';
 
 interface UserScreenProps {
   navigation: StackNavigationHelpers;
 }
 
 export default function UserScreen({ navigation }: UserScreenProps) {
+  const [intake, setIntake] = useState('');
+
   function logout() {
     storeData("user", null);
     navigation.navigate("SignIn");
+  }
+
+  function setDailyGoal() {
+    //intake
   }
 
   return (
@@ -19,6 +26,23 @@ export default function UserScreen({ navigation }: UserScreenProps) {
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Text style={{ padding: 50 }}>go back</Text>
       </TouchableOpacity>
+      <Text style={{padding:50}}>
+        Set new daily goal. 
+        Current daily goal is {/* {daily} */}
+      </Text>
+      <TextInput
+        //placeholder={daily.toString()}
+        placeholder='69420'
+        onChangeText={(text) => setIntake(text)}
+        value={String(intake)}
+        keyboardType="decimal-pad"
+      />
+      <TouchableOpacity
+          onPress={() => setDailyGoal()}
+          //style={{ ...Styles.buttonShape, ...styles.submitButton }}
+        >
+          <Text /*style={{ ...Styles.body, ...styles.submitText }}*/>Submit</Text>
+        </TouchableOpacity>
       <TouchableOpacity onPress={logout}>
         <Text style={{ padding: 50 }}>Logout</Text>
       </TouchableOpacity>

c775227373a6a968c3aeafe3cd5d4a4cc47b06bf
Annette Lau <annette1@ualberta.ca>
2021-02-06 23:29:59 -0700

    dumb thing

--- client/components/FriendsScreen.tsx
+++ client/components/FriendsScreen.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
+import { LinearGradient } from "expo-linear-gradient";
 
+import Styles from "../styles/styles";
+import Colours from "../styles/colours";
 
 interface FriendsScreenProps {
-    navigation: StackNavigationHelpers;
-  }
-  
-
-export default function FriendsScreen({ navigation }: FriendsScreenProps) {
-    return (
-        <View>
-            <Text style={{padding:50}}>This is the friends screen</Text>
-            <TouchableOpacity onPress={() => navigation.navigate("AddFriends")}>
-                <Text style={{padding:50}}>Go to add friends screen</Text>
-            </TouchableOpacity>        
-        </View>
-
-    )
+  navigation: StackNavigationHelpers;
 }
 
+export default function FriendsScreen({ navigation }: FriendsScreenProps) {
+  return (
+    <View style={Styles.screen}>
+      <LinearGradient
+        style={Styles.background}
+        colors={[Colours.darkBlue, Colours.medBlue]}
+      />
+      <Text style={Styles.title}>Friends</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("AddFriends")}>
+        <Text style={{ padding: 50 }}>Go to add friends screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}

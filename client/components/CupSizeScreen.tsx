3bea79c260a81e034116ce77401d02f20e802d7e
wchorkaw <wchorkaw@ualberta.ca>
2021-02-07 01:17:16 -0700

    asdg

--- client/components/CupSizeScreen.tsx
+++ client/components/CupSizeScreen.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import { Text, StyleSheet, View, TouchableOpacity, TextInput} from "react-native";
-=======
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
->>>>>>> aee3e9f6b88d2764bdd305f30255a1dd90cc95ed
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import { useState, useEffect } from "react";
 
+
 interface CupSizeScreenProps {
-<<<<<<< HEAD
     navigation: StackNavigationHelpers;
   }
   
@@ -71,7 +67,7 @@ export default function CupSizeScreen({ navigation }: CupSizeScreenProps) {
             </TouchableOpacity>    
             {input("Name", name, setName, false)}
             {input("Size", size, setSize, true)}
-            <TouchableOpacity onPress={() => setCups([name, size])}>
+            <TouchableOpacity onPress={() => setCups({name, size})}>
                 <Text style={{padding:50}}>Add new cup</Text>
             </TouchableOpacity> 
             {cups.map(({ name, size }) => (
@@ -86,33 +82,4 @@ export default function CupSizeScreen({ navigation }: CupSizeScreenProps) {
         ))}
         </View>
     )
-=======
-  navigation: StackNavigationHelpers;
-}
-
-const cup = (number: number) => (
-  <Text style={{ padding: 50 }}>Cup number {number}</Text>
-);
-let array: JSX.Element[] = [];
-
-export default function CupSizeScreen({ navigation }: CupSizeScreenProps) {
-  const [newCup, setNewCup] = useState(0);
-
-  useEffect(() => {
-    array.push(cup(newCup));
-    console.log(array);
-  });
-
-  return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-        <Text style={{ padding: 50 }}>This is the cup size screen</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setNewCup(newCup + 1)}>
-        <Text style={{ padding: 50 }}>Add cup</Text>
-      </TouchableOpacity>
-      {array}
-    </View>
-  );
->>>>>>> aee3e9f6b88d2764bdd305f30255a1dd90cc95ed
 }

f5d3ff2e2975f9d89989497923b5d192b4c79584
Annette Lau <annette1@ualberta.ca>
2021-02-07 01:51:52 -0700

    wack

--- client/components/CalenderScreen.tsx
+++ client/components/CalenderScreen.tsx
@@ -1,21 +1,42 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
+import { LinearGradient } from "expo-linear-gradient";
 
+import Styles from "../styles/styles";
+import Colours from "../styles/colours";
+
+import { HomeIcon } from "../assets";
 
 interface CalenderScreenProps {
-    navigation: StackNavigationHelpers;
-  }
-  
-
-export default function AddFriendsScreen({ navigation }: CalenderScreenProps) {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => navigation.navigate("Calender")}>
-                <Text style={{padding:50}}>This is the calender screen</Text>
-            </TouchableOpacity>        
-        </View>
-
-    )
+  navigation: StackNavigationHelpers;
 }
 
+export default function AddFriendsScreen({ navigation }: CalenderScreenProps) {
+  return (
+    <SafeAreaView style={Styles.screen}>
+      <LinearGradient
+        style={Styles.background}
+        colors={[Colours.darkBlue, Colours.medBlue]}
+      />
+      <View style={styles.calendarBox}></View>
+      <View style={{ ...Styles.navBar }}>
+        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+          <HomeIcon />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  calendarBox: {
+    flex: 1,
+  },
+});

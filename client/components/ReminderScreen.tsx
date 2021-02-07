0fc649deffee2e92e13f1b46c7e334af80f2c14a
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-07 00:22:27 -0700

    remove weird buttons

--- client/components/ReminderScreen.tsx
+++ client/components/ReminderScreen.tsx
@@ -21,33 +21,23 @@ interface ReminderScreenProps {
 export default function ReminderScreen({ navigation }: ReminderScreenProps) {
   const [wakeTime, setWakeTime] = useState(new Date());
   const [sleepTime, setSleepTime] = useState(new Date());
-  const [showWake, setShowWake] = useState(false);
-  const [showSleep, setShowSleep] = useState(false);
 
-  const onChangeWake = (event, selectedWake) => {
-    const currentWake = selectedWake || date;
-    setShowWake(Platform.OS === "ios");
+  const onChangeWake = (_: any, selectedWake?: Date) => {
+    const currentWake = selectedWake || new Date();
     setWakeTime(currentWake);
   };
-  const onChangeSleep = (event, selectedSleep) => {
-    const currentSleep = selectedSleep || date;
-    setShowSleep(Platform.OS === "ios");
+  const onChangeSleep = (_: any, selectedSleep?: Date) => {
+    const currentSleep = selectedSleep || new Date();
     setSleepTime(currentSleep);
   };
 
-  const showWakeMode = () => {
-    setShowWake(true);
-  };
-  const showSleepMode = () => {
-    setShowSleep(true);
-  };
-
-  const showWakeTimepicker = () => {
-    showWakeMode("time");
-  };
-  const showSleepTimepicker = () => {
-    showSleepMode("time");
-  };
+  function reminders() {
+    console.log({ wakeTime, sleepTime });
+    console.log({
+      wakeTime: wakeTime.getHours(),
+      sleepTime: sleepTime.getHours(),
+    });
+  }
 
   return (
     <View style={Styles.screen}>
@@ -62,48 +52,38 @@ export default function ReminderScreen({ navigation }: ReminderScreenProps) {
 
         {/* WAKE TIME */}
         <View style={styles.intervalBox}>
-          <TouchableOpacity
-            style={{ ...Styles.buttonShape, ...styles.wakeButton }}
-            onPress={showWakeTimepicker}
-          >
+          <View style={{ ...Styles.buttonShape, ...styles.wakeButton }}>
             <Text style={{ ...Styles.body, ...styles.wakeText }}>
               Set Wake Up Time
             </Text>
-          </TouchableOpacity>
-          {showWake && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={wakeTime}
-              mode="time"
-              is24Hour={true}
-              display="default"
-              onChange={onChangeWake}
-              textColor={Colours.medBlue}
-            />
-          )}
+          </View>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={wakeTime}
+            mode="time"
+            is24Hour={true}
+            display="default"
+            onChange={onChangeWake}
+            textColor={Colours.medBlue}
+          />
         </View>
 
         {/* SLEEP TIME */}
         <View style={styles.intervalBox}>
-          <TouchableOpacity
-            style={{ ...Styles.buttonShape, ...styles.sleepButton }}
-            onPress={showSleepTimepicker}
-          >
+          <View style={{ ...Styles.buttonShape, ...styles.sleepButton }}>
             <Text style={{ ...Styles.body, ...styles.sleepText }}>
               Set Sleep Time
             </Text>
-          </TouchableOpacity>
-          {showSleep && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={sleepTime}
-              mode="time"
-              is24Hour={true}
-              display="default"
-              onChange={onChangeSleep}
-              textColor={Colours.medBlue}
-            />
-          )}
+          </View>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={sleepTime}
+            mode="time"
+            is24Hour={true}
+            display="default"
+            onChange={onChangeSleep}
+            textColor={Colours.medBlue}
+          />
         </View>
 
         <TouchableOpacity

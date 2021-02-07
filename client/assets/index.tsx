1bea154d8ef7b414dc48d33cb5c02a5fa7be3190
Annette Lau <annette1@ualberta.ca>
2021-02-06 17:51:20 -0700

    yay index

--- client/assets/index.tsx
+++ client/assets/index.tsx
@@ -1,2 +1,12 @@
 export { default as StartIcon } from "./start_icon.svg";
 export { default as WelcomeIcon } from "./welcome_icon.svg";
+
+export { default as User } from "./user_icon.svg";
+export { default as Wapp } from "./wapp_icon.svg";
+export { default as Calendar } from "./calendar_icon.svg";
+
+export { default as Drink } from "./drink_button.svg";
+
+export { default as Home } from "./home_icon.svg";
+export { default as Tips } from "./tips_icon.svg";
+export { default as Friends } from "./friends_icon.svg";

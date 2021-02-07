142a9bc34e02797c471985841fac01995266f948
Annette Lau <annette1@ualberta.ca>
2021-02-07 01:40:04 -0700

    cupcupccupcupcu

--- client/components/LogWaterScreen.tsx
+++ client/components/LogWaterScreen.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+} from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import { LinearGradient } from "expo-linear-gradient";
 
 import Styles from "../styles/styles";
 import Colours from "../styles/colours";
-import { ScrollView, TextInput } from "react-native-gesture-handler";
 
 import { HomeIcon } from "../assets";
 
@@ -25,7 +31,7 @@ export default function ReminderScreen({ navigation }: LogWaterScreenProps) {
       </Text>
       <View style={styles.manualBox}>
         <Text style={{ ...Styles.body, ...styles.smallText }}>
-          Manually add volume of water:
+          Manually add litres of water:
         </Text>
         <TextInput
           style={styles.waterInput}
@@ -36,11 +42,19 @@ export default function ReminderScreen({ navigation }: LogWaterScreenProps) {
       <View style={styles.cupList}>
         <ScrollView keyboardDismissMode="on-drag">
           <TouchableOpacity
+            style={{ ...Styles.buttonShape, ...styles.addCupButton }}
+            onPress={() => navigation.navigate("CupSize")}
+          >
+            <Text style={{ ...Styles.body, ...styles.addCupText }}>
+              Add a cup size +
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
             style={{ ...Styles.buttonShape, ...styles.cupButton }}
             onPress={() => navigation.navigate("CupSize")}
           >
             <Text style={{ ...Styles.body, ...styles.cupText }}>
-              Add a cup size
+              Small: 0.1 L
             </Text>
           </TouchableOpacity>
         </ScrollView>
@@ -68,6 +82,16 @@ const styles = StyleSheet.create({
     padding: 10,
     // borderWidth: 1,
   },
+
+  addCupButton: {
+    borderColor: Colours.yellow,
+    borderWidth: 1,
+  },
+  addCupText: {
+    textAlign: "center",
+    color: Colours.yellow,
+  },
+
   cupButton: {
     backgroundColor: Colours.yellow,
   },
@@ -75,6 +99,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: Colours.darkBlue,
   },
+
   title: {
     textAlign: "center",
     marginTop: 30,

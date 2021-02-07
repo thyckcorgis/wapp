aee3e9f6b88d2764bdd305f30255a1dd90cc95ed
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-07 01:13:03 -0700

    refactor api

--- client/api.tsx
+++ client/api.tsx
@@ -1,125 +1,57 @@
 import fetch from "axios";
 import { API_URL } from "./constants";
 
+function post(path: string, data: any) {
+  return fetch({
+    url: API_URL + path,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    method: "POST",
+    data,
+  });
+}
 export async function registerUser(
   username: string,
   password: string,
   name: string,
   daily: number
 ) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/user/register`,
-    data: { username, password, name, daily },
-    method: "POST",
-  });
+  const userData = { username, password, name, daily };
+  const { data } = await post("/user/register", userData);
   return data;
+}
+
+export async function uploadPushToken(username: string, expoPushToken: string) {
+  return (await post("/user/notif", { username, expoPushToken })).data.user;
 }
 
 export async function setDailyIntake(username: string, daily: number) {
   const userData = { username, daily };
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/user/daily`,
-    data: userData,
-    method: "POST",
-  });
-  return data;
+  return (await post("/user/daily", userData)).data;
 }
 
 export async function loginUser(username: string, password: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/user/login`,
-    data: { username, password },
-    method: "POST",
-  });
-  return data;
+  return (await post("/user/login", { username, password })).data;
 }
 
 export async function getPendingRequests(username: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/friend/pending`,
-    data: { username },
-    method: "POST",
-  });
-  return data.pending;
+  return (await post("/friend/pending", { username })).data.pending;
 }
 
 export async function sendFriendRequest(username: string, friend: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/friend/request`,
-    data: { username, friend },
-    method: "POST",
-  });
-  return data;
+  return (await post("/friend/request", { username, friend })).data;
 }
 
 export async function acceptFriendRequest(username: string, friend: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/friend/accept`,
-    data: { username, friend },
-    method: "POST",
-  });
-  return data;
+  return (await post("/friend/accept", { username, friend })).data;
 }
 
 export async function getNonFriends(username: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/friend/toadd`,
-    data: { username },
-    method: "POST",
-  });
-  return data.users;
+  return (await post("/friend/toadd", { username })).data.users;
 }
 
 export async function getFriends(username: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/friend`,
-    data: { username },
-    method: "POST",
-  });
-  return data.users;
-}
-
-export async function uploadPushToken(username: string, expoPushToken: string) {
-  const { data } = await fetch({
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    url: `${API_URL}/user/notif`,
-    data: { username, expoPushToken },
-    method: "POST",
-  });
-  return data.user;
+  return (await post("/friend", { username })).data.users;
 }

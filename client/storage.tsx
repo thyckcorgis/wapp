4ad7f45a94cc736bd2e758599d90013bb157cece
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 21:46:32 -0700

    remove console log from storage

--- client/storage.tsx
+++ client/storage.tsx
@@ -3,7 +3,6 @@ import { AsyncStorage } from "react-native";
 export async function storeData(key: string, value: object | null) {
   try {
     const jsonValue = JSON.stringify(value);
-    console.log(jsonValue);
     await AsyncStorage.setItem(key, jsonValue);
   } catch (e) {
     console.log(e);
@@ -13,7 +12,6 @@ export async function storeData(key: string, value: object | null) {
 export async function getData(key: string): Promise<object | undefined> {
   try {
     const jsonValue = await AsyncStorage.getItem(key);
-    console.log(jsonValue);
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (e) {
     console.log(e);

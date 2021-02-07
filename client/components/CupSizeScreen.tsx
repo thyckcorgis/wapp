875dbcef08f2e3413d9c1f168a58072b9dfeb374
wchorkaw <wchorkaw@ualberta.ca>
2021-02-07 01:13:55 -0700

    almost there

--- client/components/CupSizeScreen.tsx
+++ client/components/CupSizeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, TextInput} from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import { useState, useEffect } from "react";
 
@@ -8,31 +8,78 @@ interface CupSizeScreenProps {
     navigation: StackNavigationHelpers;
   }
   
+  interface Cup {
+      name: string,
+      size: string
+  }
 
-  const cup = (
-    number: number
-) => (
-    <Text style={{padding:50}}>Cup number {number}</Text>
-)
-  let array: JSX.Element[] = []
+  const textField = (
+    placeholder: string,
+    value: string,
+    setValue: React.Dispatch<React.SetStateAction<string>>,
+    numberPad: boolean
+  ) => (
+    <TextInput
+      //style={{ ...Styles.inputField, ...styles.inputField }}
+      placeholder={placeholder}
+     // placeholderTextColor={Colours.medBlue}
+      onChangeText={(text) => setValue(text)}
+      value={value}
+      keyboardType={numberPad ? "decimal-pad" : "default"}
+    />
+  );
+
+  const input = (
+    placeholder: string,
+    value: string,
+    setValue: React.Dispatch<React.SetStateAction<string>>,
+    numberPad: boolean
+    //style?:
+  ) => (
+    <View>
+      <Text>
+        {placeholder + ":"}
+      </Text>
+      {textField(placeholder, value, setValue, numberPad)}
+    </View>
+  );
 
 export default function CupSizeScreen({ navigation }: CupSizeScreenProps) {
-    const [newCup, setNewCup] = useState(0)
+    const [cups, setCups] = useState<Cup[]>([]);
+    const [name, setName] = useState('');
+    const [size, setSize] = useState('');
 
-    useEffect(() => {
-        array.push(cup(newCup))
-        console.log(array)
-    })
+   
+    // function addFriend(friend: string) {
+    //     return () => {
+    //       (async () => {
+    //         const data = await sendFriendRequest(username, friend);
+    //         console.log(data);
+    //       })();
+    //     };
+    //   }
 
+    
     return (
         <View>
             <TouchableOpacity onPress={() => navigation.navigate("Home")}>
                 <Text style={{padding:50}}>This is the cup size screen</Text>
             </TouchableOpacity>    
-            <TouchableOpacity onPress={() => setNewCup(newCup+1)}>
-                <Text style={{padding:50}}>Add cup</Text>
+            {input("Name", name, setName, false)}
+            {input("Size", size, setSize, true)}
+            <TouchableOpacity onPress={() => setCups([name, size])}>
+                <Text style={{padding:50}}>Add new cup</Text>
             </TouchableOpacity> 
-            { array }
+            {cups.map(({ name, size }) => (
+          <View style={{padding:50}} key={name}>
+            <Text>
+              {name}: {size} mL
+            </Text>
+            {/* <TouchableOpacity onPress={() => addCup(name)()}>
+              <Text>Add Friend</Text>
+            </TouchableOpacity> */}
+          </View>
+        ))}
         </View>
     )
 }

25a6a1f4eb91c27f80cbaefb5e69583767c1e742
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-06 23:33:54 -0700

    add friend screem

--- client/components/FriendsScreen.tsx
+++ client/components/FriendsScreen.tsx
@@ -1,22 +1,33 @@
-import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
-
+import { getFriends } from "../api";
+import { getData } from "../storage";
 
 interface FriendsScreenProps {
-    navigation: StackNavigationHelpers;
-  }
-  
-
-export default function FriendsScreen({ navigation }: FriendsScreenProps) {
-    return (
-        <View>
-            <Text style={{padding:50}}>This is the friends screen</Text>
-            <TouchableOpacity onPress={() => navigation.navigate("AddFriends")}>
-                <Text style={{padding:50}}>Go to add friends screen</Text>
-            </TouchableOpacity>        
-        </View>
-
-    )
+  navigation: StackNavigationHelpers;
+}
+interface User {
+  username: string;
+  name: string;
 }
 
+export default function FriendsScreen({ navigation }: FriendsScreenProps) {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const user = (await getData("user")) as User;
+      if (user == null) return navigation.navigate("SignIn");
+      const friends = await getFriends(user.username);
+      setFriends(friends);
+    })();
+  }, [setFriends]);
+  return (
+    <View>
+      <Text style={{ padding: 50 }}>This is the friends screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("AddFriends")}>
+        <Text style={{ padding: 50 }}>Go to add friends screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}

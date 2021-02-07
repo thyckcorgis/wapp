d78010c5f8b0319613e443e7c094fcb5ab937d91
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-07 00:55:45 -0700

    update intake should work now

--- client/components/UserScreen.tsx
+++ client/components/UserScreen.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Text, StyleSheet, View, TouchableOpacity, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import { getData, storeData } from "../storage";
-import { useState } from 'react';
-import { setDailyIntake } from '../api'
+import { setDailyIntake } from "../api";
 
 interface UserScreenProps {
   navigation: StackNavigationHelpers;
@@ -11,41 +10,37 @@ interface UserScreenProps {
 
 interface User {
   username: string;
-  daily: string;
+  daily: number;
 }
 
 export default function UserScreen({ navigation }: UserScreenProps) {
-  const [newIntake, setNewIntake] = useState('');
-  const [intake, setIntake] = useState('');
+  const [newIntake, setNewIntake] = useState("");
+  const [username, setUsername] = useState("");
+  const [intake, setIntake] = useState("");
 
-  async function getUser() {
-    const user = await getData('user') as User;
-    if (user == null) {
-      return;
-    } else {
-    const { daily, username } = user
-    setIntake(daily)    
-    return username;
-    }
-  }
-  
-
-  function logout() {
-    storeData("user", null);
+  async function logout() {
+    await storeData("user", null);
     navigation.navigate("SignIn");
   }
 
-    //const [intake, setIntake] = useState(`${daily}`);
-    async function updateIntake() {
-      const username = getUser();
-      const data = await setDailyIntake(username.toString(), Number(newIntake));
-      if (!data.ok) {
-        console.log(data.messsage);
-      } else {
-        storeData("user", data.user);
-        navigation.navigate("Reminder");
-      }
-    }  
+  async function updateIntake() {
+    const data = await setDailyIntake(username, Number(newIntake));
+    if (!data.ok) {
+      console.log(data.messsage);
+    } else {
+      storeData("user", data.user);
+      navigation.navigate("Reminder");
+    }
+  }
+  useEffect(() => {
+    (async () => {
+      const user = (await getData("user")) as User;
+      if (!user) return navigation.navigate("SignIn");
+
+      setUsername(user.username);
+      setIntake(user.daily.toFixed(1));
+    })();
+  }, [setIntake, setUsername]);
 
   return (
     <View>
@@ -53,10 +48,10 @@ export default function UserScreen({ navigation }: UserScreenProps) {
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Text style={{ padding: 50 }}>go back</Text>
       </TouchableOpacity>
-      <Text style={{padding:50}}>
-        Set new daily goal. 
-        Current daily goal is {intake}
+      <Text style={{ padding: 50 }}>
+        Set new daily goal. Current daily goal is {intake}
       </Text>
+
       <TextInput
         placeholder={intake}
         onChangeText={(text) => setNewIntake(text)}
@@ -64,11 +59,11 @@ export default function UserScreen({ navigation }: UserScreenProps) {
         keyboardType="decimal-pad"
       />
       <TouchableOpacity
-          onPress={() => updateIntake()}
-          //style={{ ...Styles.buttonShape, ...styles.submitButton }}
-        >
-          <Text /*style={{ ...Styles.body, ...styles.submitText }}*/>Submit</Text>
-        </TouchableOpacity>
+        onPress={() => updateIntake()}
+        //style={{ ...Styles.buttonShape, ...styles.submitButton }}
+      >
+        <Text /*style={{ ...Styles.body, ...styles.submitText }}*/>Submit</Text>
+      </TouchableOpacity>
       <TouchableOpacity onPress={logout}>
         <Text style={{ padding: 50 }}>Logout</Text>
       </TouchableOpacity>

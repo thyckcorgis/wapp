473c422911a832ae07b15e8c825867088e90b050
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-02-13 00:59:53 -0700

    add local change

--- client/constants.ts
+++ client/constants.ts
@@ -1,5 +1,8 @@
 export const API_URL = "https://thyck.top/wapee";
 
+// if you're working locally
+// export const API_URL = "http://10.0.0.58:5003/wapee";
+
 export const defaultCups = [
   {
     name: "Small cup",

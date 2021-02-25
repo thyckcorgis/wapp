b6fbace8b4208d3f12e40f9a103ccc8d74558c34
Annette Lau <annette1@ualberta.ca>
2021-02-24 18:28:54 -0700

    add button time to try things out ugh

---
+++ client/components/buttons/addButton.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Colours } from "../../styles";
+
+type AddButtonProps = {
+  onAdd: () => void;
+};
+
+export default function addButton({ onAdd }: AddButtonProps) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onAdd}>
+      <Text style={styles.plus}>+</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 55,
+    height: 55,
+    borderRadius: 28,
+    borderWidth: 1,
+    borderColor: Colours.yellow,
+    position: "absolute",
+    right: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 20,
+    zIndex: 1,
+    elevation: 1,
+  },
+  plus: {
+    color: Colours.yellow,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});

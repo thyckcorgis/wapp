c89824de5bb329f12b00528c6e7535a740593d77
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 15:19:27 -0700

    finish LogService

--- server/src/services/LogService.ts
+++ server/src/services/LogService.ts
@@ -1,5 +1,5 @@
 import User from "../models/user";
-import Log from "../models/log";
+import Log, { LogType } from "../models/log";
 
 import { ILog } from "../util/types";
 import * as validators from "../util/validations";
@@ -10,31 +10,44 @@ function createMessage(username: string, intake: number, goalMet: boolean) {
   return `${username} just drank ${intake} mL of water!`;
 }
 
+const sum = (total: number, num: number) => total + num;
+
+const getTodaysIntake = (logs: ILog[]) => {
+  const today = new Date().setHours(0, 0, 0, 0);
+  const tomorrow = today + 1000 * 60 * 60 * 24;
+  return logs
+    .filter((log) => today <= log[1] && log[1] < tomorrow)
+    .map(([intake]) => intake)
+    .reduce(sum);
+};
+
 export async function syncLogs(userId: string, logs: ILog[]) {
-  try {
-    const { error } = validators.syncLogs.validate(logs);
-    if (error) throw error;
-    const user = await User.findById(userId).exec();
-    if (!user) throw new Error("User not found");
-  } catch (err) {
-    throw err;
-  }
+  const { error } = validators.syncLogs.validate(logs);
+  if (error) throw error;
+
+  const currentIntake = getTodaysIntake(logs);
+  await User.findByIdAndUpdate(userId, { currentIntake }).exec();
+
+  const logType: LogType = "water";
+  await Log.insertMany(
+    logs.map(([water, date]) => ({ userId, water, dateCreated: new Date(date), logType }))
+  );
 }
 
 export async function logWater(username: string, userId: string, intake: number) {
-  try {
-    const { error } = validators.logIntake.validate({ intake });
-    if (error) throw error;
-    const user = await User.findById(userId).exec();
-    if (!user) throw new Error("User not found");
+  const { error } = validators.logIntake.validate({ intake });
+  if (error) throw error;
+  const user = await User.getUser(userId);
+  const message = createMessage(username, intake, await user.addWater(intake));
 
-    const message = createMessage(username, intake, await user.addWater(intake));
-    const newLog = new Log({ userId, water: intake });
-    await newLog.save();
-    const pushTokens = (await user.getFriends()).map((friend) => friend.pushTokens).flat();
-    sendNotifications(message, pushTokens);
-    return newLog.water;
-  } catch (err) {
-    throw err;
-  }
+  const newLog = new Log({ userId, water: intake });
+  await newLog.save();
+
+  const friendPushTokens = (await user.getFriends()).map((friend) => friend.pushTokens).flat();
+  sendNotifications(message, friendPushTokens);
+  return newLog.water;
+}
+
+export async function logFriendRequest(userId: string, friendId: string) {
+  await new Log({ userId, friendId, logType: "friend" }).save();
 }

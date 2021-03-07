267f527acf6065d910b8008d2ebd2c2d88c970bc
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 16:34:05 -0700

    add EDate to types

--- server/src/util/types.ts
+++ server/src/util/types.ts
@@ -3,6 +3,24 @@ export interface UserData {
   userId: string;
 }
 
+// date is in seconds
 export type ILog = [water: number, date: number];
 
 export type UserType = "friends" | "nonFriends" | "pending";
+
+export class EDate extends Date {
+  get month() {
+    return this.getMonth() + 1;
+  }
+  get day() {
+    return this.getDate();
+  }
+  get year() {
+    return this.getFullYear();
+  }
+  get serial() {
+    return Math.floor(this.getTime() / 1000);
+  }
+}
+
+export type LogType = "water" | "friend";

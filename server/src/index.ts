7cc19313690e24f3d73a629189ba6ee52e4d8feb
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 07:36:29 -0700

    clean index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,9 @@
 import Express from "express";
 
-import { userRouter, logRouter, friendRouter } from "./routes";
-
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+import router from "./routes";
 
 const app = Express();
 const port = 5003;
-
-const router = Express.Router();
 
 app.use(Express.json());
 
@@ -15,13 +11,6 @@ app.use((req, _, next) => {
   const { url, method } = req;
   console.log({ url, method });
   next();
-});
-router.use("/user", userRouter);
-router.use("/log", logRouter);
-router.use("/friend", friendRouter);
-
-router.get("/", (_, res) => {
-  res.send("Hello pee");
 });
 
 app.use("/wapee", router);
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,3 +1,21 @@
-export { default as friendRouter } from "./friends";
-export { default as logRouter } from "./logs";
-export { default as userRouter } from "./users";
+import { Router } from "express";
+
+import friendRouter from "./friends";
+import logRouter from "./logs";
+import userRouter from "./users";
+import sessionRouter from "./session";
+import notificationRouter from "./notification";
+
+const router = Router();
+
+router.use("/notification", notificationRouter);
+router.use("/session", sessionRouter);
+router.use("/user", userRouter);
+router.use("/log", logRouter);
+router.use("/friend", friendRouter);
+
+router.get("/", (_, res) => {
+  res.send("Hello pee");
+});
+
+export default router;

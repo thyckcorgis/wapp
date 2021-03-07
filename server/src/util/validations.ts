e34dae1391a2524347356081980bc50c8ca9a7f5
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 07:26:49 -0700

    add syncLogs validator

--- server/src/util/validations.ts
+++ server/src/util/validations.ts
@@ -8,9 +8,13 @@ const name = Joi.string().min(1).required();
 const time = Joi.number().min(0).max(23).required();
 
 // this is how much water you can log at one time
-const water = Joi.number().min(0).max(2000).required();
+const intake = Joi.number().min(0).max(2000).required();
+// for log sync
+const date = Joi.date().required();
+const logEntry = Joi.object().length(2).keys({ 0: intake, 1: date });
 
-export const logIntake = Joi.object().keys({ water });
+export const syncLogs = Joi.array().items(logEntry);
+export const logIntake = Joi.object().keys({ intake });
 export const setReminders = Joi.object().keys({ wakeTime: time, sleepTime: time });
 export const setDailyIntake = Joi.object().keys({ daily });
 export const register = Joi.object().keys({ username, daily, password, name });

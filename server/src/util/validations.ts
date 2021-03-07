f154400237de776d900dc67647e1198c8aa27be1
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 06:49:12 -0700

    add water validation + turn daily into mL

--- server/src/util/validations.ts
+++ server/src/util/validations.ts
@@ -1,11 +1,16 @@
 import Joi from "joi";
 
 const username = Joi.string().alphanum().min(6).max(30).required();
-const daily = Joi.number().min(1).max(25).required();
+const daily = Joi.number().min(1000).max(25000).required();
 const password = Joi.string().min(8).required();
+
 const name = Joi.string().min(1).required();
 const time = Joi.number().min(0).max(23).required();
 
+// this is how much water you can log at one time
+const water = Joi.number().min(0).max(2000).required();
+
+export const logIntake = Joi.object().keys({ water });
 export const setReminders = Joi.object().keys({ wakeTime: time, sleepTime: time });
 export const setDailyIntake = Joi.object().keys({ daily });
 export const register = Joi.object().keys({ username, daily, password, name });

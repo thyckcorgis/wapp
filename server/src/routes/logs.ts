647a3e9aa9f0cffbb7fe578b3f0c1c05259236cf
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 16:38:06 -0700

    finish get log route

--- server/src/routes/logs.ts
+++ server/src/routes/logs.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { checkAuth, AuthReq } from "../middlewares";
-import { UserData } from "../util/types";
+import { UserData, EDate } from "../util/types";
 import * as LogService from "../services/LogService";
 import { parseError } from "../util/helpers";
+import { getMonthly } from "src/util/validations";
 
 const logRouter = Router();
 
@@ -21,6 +22,22 @@ logRouter.post("", checkAuth, async ({ body, userData }: AuthReq, res) => {
     const { userId, username } = userData as UserData;
     const { water } = body;
     res.send(await LogService.logWater(username, userId, water));
+  } catch (err) {
+    res.status(400).send(parseError(err));
+  }
+});
+
+const getNum = (num: any) => parseInt(num);
+logRouter.get("", checkAuth, async ({ query, userData }: AuthReq, res) => {
+  try {
+    let year = getNum(query.year),
+      month = getNum(query.month);
+    // use this month if invalid input
+    const { error } = getMonthly.validate({ year, month });
+    if (error) ({ year, month } = new EDate());
+
+    const { userId } = userData as UserData;
+    res.send(await LogService.getMonthlyLog(userId, year, month));
   } catch (err) {
     res.status(400).send(parseError(err));
   }

a3192807124c664c216d8d5cf4b1bfbec375c80d
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-07 17:06:26 -0700

    add validator function

--- server/src/util/validations.ts
+++ server/src/util/validations.ts
@@ -10,13 +10,22 @@ const time = Joi.number().min(0).max(23).required();
 // this is how much water you can log at one time
 const intake = Joi.number().min(0).max(2000).required();
 // for log sync
-const date = Joi.date().required();
+const date = Joi.number().integer().required();
 const logEntry = Joi.object().length(2).keys({ 0: intake, 1: date });
 
+const year = Joi.number().integer().min(1970).max(2999).required();
+const month = Joi.number().integer().min(1).max(12).required();
+
+export const getMonthly = Joi.object().keys({ year, month });
 export const addFriend = Joi.object().keys({ friend: username });
 export const syncLogs = Joi.array().items(logEntry);
 export const logIntake = Joi.object().keys({ intake });
 export const setReminders = Joi.object().keys({ wakeTime: time, sleepTime: time });
-export const setDailyIntake = Joi.object().keys({ daily });
+export const setIntake = Joi.object().keys({ daily });
 export const register = Joi.object().keys({ username, daily, password, name });
 export const login = Joi.object().keys({ username, password });
+
+export async function validate<T>(schema: Joi.Schema<T>, data: T) {
+  const { error } = schema.validate(data);
+  if (error) throw error;
+}

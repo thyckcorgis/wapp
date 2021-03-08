4a6c7bcc6bb0fbb52c28f1313080aa978e9dbc49
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-08 02:09:47 -0700

    remove percentage + remove duplicate line

--- server/src/services/FriendsService.ts
+++ server/src/services/FriendsService.ts
@@ -11,23 +11,23 @@ const completion = (b: IUserDocument, a: IUserDocument) =>
 const percentage = (user: IUserDocument) => ({
   username: user.username,
   name: user.name,
-  currrentIntake: user.currentIntake,
+  currentIntake: user.currentIntake,
   daily: user.daily,
-  percentage: (user.currentIntake / user.daily) * 100,
 });
 
 async function getFriend(friend: string) {
   await validate(addFriend, { friend });
   try {
     return User.findByUsername(friend);
-  } catch (e) {
+  } catch {
     throw new Error("Friend not found");
   }
 }
 
 async function makeConnection(user: IUserDocument, friend: IUserDocument, send = true) {
   await user.update({ $push: { friendIds: friend._id } }).exec();
-  if (friend.pushTokens.length) friendRequestNotification(user.username, friend.pushTokens, send);
+  if (friend.notify && friend.pushTokens.length)
+    friendRequestNotification(user.username, friend.pushTokens, send);
   await new Log({ userId: user._id, friendId: friend._id, logType: "friend" }).save();
 }
 
@@ -55,14 +55,15 @@ export async function AcceptRequest(userId: string, friendName: string) {
   const friend = await getFriend(friendName);
   const user = (await friend.getFriends()).find((user) => user._id === userId);
   if (!user) throw new Error("You are not in this user's friend list");
-  makeConnection(user, friend);
+  makeConnection(user, friend, false);
 }
+// Delete a friend
+// Cancel friend request
 
 export async function SendRequest(userId: string, friendName: string) {
   const friend = await getFriend(friendName);
   const user = await User.getUser(userId);
   const existingFriend = (await user.getFriends()).find((user) => user._id === friend._id);
   if (existingFriend) throw new Error("Request already sent");
-  await user.update({ $push: { friendIds: friend._id } }).exec();
-  makeConnection(user, friend, false);
+  makeConnection(user, friend);
 }

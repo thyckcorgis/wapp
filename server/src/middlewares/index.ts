e17171b4aa2a9b76936064d8c12458a54702d8cf
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-08 02:10:08 -0700

    use config instead of env

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+import { JWT_KEY } from "../config";
 import { UserData } from "../util/types";
 
 export interface AuthReq extends Request {
@@ -12,7 +13,7 @@ export function checkAuth(req: AuthReq, res: Response, next: NextFunction) {
     const authHeader = req.headers["authorization"];
     const token = authHeader?.split(" ")[1];
 
-    req.userData = jwt.verify(token as string, process.env.JWT_KEY as string) as UserData;
+    req.userData = jwt.verify(token as string, JWT_KEY) as UserData;
     next();
   } catch (err) {
     res.status(401).send("Auth failed");

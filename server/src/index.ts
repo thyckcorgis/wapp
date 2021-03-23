82450e117a5db6c7c5d738b2b3f2137839e97d26
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-23 01:19:18 -0600

    move everything to loaders

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,13 @@
 import "reflect-metadata";
 
 import express from "express";
-import { logUrlAndMethod } from "./middlewares";
 
-import router from "./routes";
 import { loadDependencies } from "./loaders";
 
-const app = express();
-const port = 5003;
-
 export const startServer = async () => {
-  await loadDependencies();
-  app.disable("x-powered-by");
-  app.use(express.json());
-  app.use(logUrlAndMethod);
-  app.use("/wapee", router);
+  const app = express();
+  const port = 5003;
+  await loadDependencies(app);
   app.listen(port, () => console.log(`Listening on port ${port}`));
 };
 
--- server/src/loaders/index.ts
+++ server/src/loaders/index.ts
@@ -1,14 +1,22 @@
 import { Container } from "typedi";
+import { json, Express } from "express";
 
 import { EXPIRES_IN, JWT_KEY } from "../config";
 import { AuthHelper } from "../util/auth";
 
+import { logUrlAndMethod } from "../middlewares";
+
 import { LogRepo, UserRepo } from "../data";
 import loadDataLayer from "../data/loader";
 
-export async function loadDependencies() {
+export async function loadDependencies(app: Express) {
   Container.set<AuthHelper>("auth", new AuthHelper(JWT_KEY, EXPIRES_IN));
   const { userRepo, logRepo } = await loadDataLayer("json");
   Container.set<UserRepo>("userRepo", userRepo);
   Container.set<LogRepo>("logRepo", logRepo);
+
+  app.disable("x-powered-by");
+  app.use(json());
+  app.use(logUrlAndMethod);
+  app.use("/wapee", require("../routes").default);
 }

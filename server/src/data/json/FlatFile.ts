e2437df77b7f4a5b309996d254bfe19dad12f47c
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-03-22 23:40:57 -0600

    add flatfile interface + helpers

---
+++ server/src/data/json/FlatFile.ts
@@ -0,0 +1,25 @@
+import { writeFile, readFile } from "fs";
+
+export type FileObject<T> = { [x: string]: T };
+
+export const saveToFile = <T>(path: string, items: FileObject<T>) =>
+  new Promise<void>((resolve, reject) => {
+    writeFile(path, JSON.stringify(items, null, 2), (err) => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+
+export const loadFromFile = <T>(path: string) =>
+  new Promise<FileObject<T>>((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) return reject(err);
+      const readData = JSON.parse(data.toString());
+      resolve(readData as FileObject<T>);
+    });
+  });
+
+export default interface FlatFile {
+  load(): Promise<void>;
+  save(): Promise<void>;
+}

725984139f7ecfff5972bfce83a05f28bfa0245b
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-05-14 02:32:25 -0600

    Rearrange user declaration

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :logs
 
+  has_many :following_users, foreign_key: :followee_id, class_name: "Follow"
   has_many :followers, through: :following_users
-  has_many :following_users, foreign_key: :user_id, class_name: "Follow"
 
+  has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
   has_many :followees, through: :followed_users
-  has_many :followed_users, foreign_key: :user_id, class_name: "Follow"
 end
